dc31c5bcae9d0771f797c866e2c85e61e2c2367f
domsoos <soos.domi@gmail.com>
2020-07-22 10:59:57 -0400

    nav-bar
    navigation bar skeleton and changes in App.js

--- app/src/App.js
+++ app/src/App.js
@@ -6,16 +6,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+          <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+
           <h2>Welcome to Violent Tracker</h2>
+
         </div>
         <p className="App-Intro">
           This is our app that tracks police violence in a map
         </p>
+        
+
       </div>
     );
   }
 }
-
 export default App;
+
